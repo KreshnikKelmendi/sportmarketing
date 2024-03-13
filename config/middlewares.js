cf8e8b33f7430cf2a0294c76656256b4e7600070
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2024-03-13 00:12:11 -1200

    green

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,20 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+              'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+              upgradeInsecureRequests: null,
+            },
+          },
         },
       },
-    },
-  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
